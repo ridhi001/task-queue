2a19dec0f51e7cda0b9dece1d2057c98a98c25ba
ridhi001 <kumariridhi1906i@gmail.com>
2026-05-17 18:07:38 +0530

    Initial project setup

---
+++ src/test/java/com/taskqueue/task_queue/TaskQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskqueue.task_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
